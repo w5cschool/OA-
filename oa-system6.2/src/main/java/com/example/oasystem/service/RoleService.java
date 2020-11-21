d31ee087080735a90985823fd527e552c09555e4
w5school <1582485954@qq.com>
2020-11-21 13:49:32 +0800

    :wq! wq!

---
+++ oa-system6.2/src/main/java/com/example/oasystem/service/RoleService.java
@@ -0,0 +1,45 @@
+package com.example.oasystem.service;
+
+import com.example.oasystem.entity.Role;
+import com.example.oasystem.entity.RoleExample;
+import com.example.oasystem.mapper.RoleMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author tianyaliaowang
+ * @date 2020/11/12 - 20:34
+ */
+@Service
+public class RoleService {
+
+    @Resource
+    RoleMapper roleMapper;
+    /**
+     * 查询角色列表
+     * @param startNum
+     * @param size
+     * @return
+     */
+    public PageInfo<Role> findRoleList(int startNum, int size) {
+
+        PageHelper.startPage(startNum,size);
+        RoleExample roleExample = new RoleExample();
+
+        List<Role> list = roleMapper.selectByExample(roleExample);
+
+        return new PageInfo<>(list,2);
+
+    }
+
+    public Role findRoleByRoleId(int roleId) {
+
+        return roleMapper.selectByPrimaryKey(roleId);
+
+    }
+}
